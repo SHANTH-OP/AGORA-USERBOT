0aa68865debd7ecd56d750364c3a505ac4e888fe
PROFESSOR AGORA <98295372+ProfessorAgora@users.noreply.github.com>
2022-01-25 22:18:59 +0530

    Update updater.py

--- setup/updater.py
+++ setup/updater.py
@@ -42,7 +42,7 @@ async def update_requirements(main , test):
     try:
         for i in a:
             await runcmd(f"pip install {i}")
-            print(f"🔥🗡 DARKLEGEND-Pro 🔥🗡 {i}")
+            print(f"☘️ AGORA USERBOT ☘️  {i}")
     except Exception as e:
         print(f"Error while installing requirments {str(e)}")
 
